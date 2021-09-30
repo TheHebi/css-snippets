9b4262ec1ff889c8f5e1ff7675b1d01e621647cc
TheHebi <nturcotte8@live.com>
2021-09-30 12:47:52 -0700

    basic styling, copy text to clipboard button functionality, copy text button for each snippet

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,9 +22,35 @@ const changeTab = function (event) {
       document.getElementById("algs").classList.remove("hidden");
     }
   };
-  
+
+const copyText = async function(event){
+  try{
+    console.log(event.target.parentElement.children[2].children[0].textContent)
+    const text = event.target.parentElement.children[2].children[0].textContent
+    await navigator.clipboard.writeText(text)
+    alert("Snippet Copied To Clipboard")
+  }catch(err){
+    alert("Error Occured", err)
+  }
+}
+
+
+
+const addClick = function(){
+  const copyBtn = document.querySelectorAll('.copy-text')
+console.log(copyBtn)
+  for (let i = 0; i < copyBtn.length; i++) {
+    copyBtn[i].addEventListener('click', copyText)
+    console.log(copyBtn[1])
+    
+  }
+}
+
+addClick()
+
   // Adds event listeners to each tab button
   document.querySelector("#cssTab").addEventListener("click", changeTab);
   document.querySelector("#bsTab").addEventListener("click", changeTab);
   document.querySelector("#jsTab").addEventListener("click", changeTab);
   document.querySelector("#algTab").addEventListener("click", changeTab);
+
